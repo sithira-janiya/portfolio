14e12a36021c0d32c52c02beb1674bcccecbee1a
sithira-janiya <sithirajaniya87@gmail.com>
2026-05-07 09:20:00 +0530

    feat: add Certificates component to App layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Certificates from "./components/Certificates";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
@@ -13,6 +14,7 @@ function App() {
       <Hero />
       <About />
       <Skills />
+      <Certificates />
       <Projects />
       <Contact />
       <Footer />
