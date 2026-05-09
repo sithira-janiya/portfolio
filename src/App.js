c1f6ab32b6422d8818f137f6521c0453eaede771
sithira-janiya <sithirajaniya87@gmail.com>
2026-05-09 21:13:20 +0530

    feat: simplify App component by integrating PortfolioShowcase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Certificates from "./components/Certificates";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import "./App.css";
+import PortfolioShowcase from "./components/PortfolioShowcase";
 
 function App() {
-  return (
-    <div className="font-sans">
-      <Navbar />
-      <Hero />
-      <About />
-      <Skills />
-      <Certificates />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
-  );
+  return <PortfolioShowcase />;
 }
 
 export default App;
